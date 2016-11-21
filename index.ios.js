55998d00ac814fd076736020d4e3d31f2432b8aa
erikjuhani <erik.kinn@gmail.com>
2016-11-21 13:42:35 +0200

    Fixed Activity class, and Pie view.

--- index.ios.js
+++ index.ios.js
@@ -21,38 +21,40 @@ var Activity = function(id, category, startDate, endDate) {
     this.id = id;
     this.startDate = startDate;
     this.endDate = endDate;
-    this.duration = moment.duration(this.endDate.diff(this.startDate)).asMilliseconds();
-    this.multiple = false;
+    if(startDate && endDate) {
+      this.duration = moment.duration(this.endDate.diff(this.startDate)).asMilliseconds();
+      this.multiple = false;
 
-    if(!(this.startDate.isSame(this.endDate.format(), 'day'))) {
-      var s = this.startDate.clone().endOf('day').format(),
-          e = this.endDate.clone().startOf('day').format();
-      this.dur0 = moment.duration(moment(s).diff(this.startDate)).asMilliseconds();
-      this.dur1 = moment.duration(this.endDate.diff(e)).asMilliseconds();
-      this.multiple = true;
+      if(!(this.startDate.isSame(this.endDate.format(), 'day'))) {
+        var s = this.startDate.clone().endOf('day').format(),
+            e = this.endDate.clone().startOf('day').format();
+        this.dur0 = moment.duration(moment(s).diff(this.startDate)).asMilliseconds();
+        this.dur1 = moment.duration(this.endDate.diff(e)).asMilliseconds();
+        this.multiple = true;
 
-      var key0 = this.startDate.format('YYYY-MM-DD');
-        if(days[key0]) {
-          days[key0].activities.push(this);
-        } else {
-          days[key0] = new Day(key0);
-          days[key0].activities.push(this);
-        }
-      var key1 = this.endDate.format('YYYY-MM-DD');
-        if(days[key1]) {
-          days[key1].activities.push(this);
-        } else {
-          days[key1] = new Day(key1);
-          days[key1].activities.push(this);
-        }
-    } else {
-      var key0 = this.startDate.format('YYYY-MM-DD');
-        if(days[key0]) {
-          days[key0].activities.push(this);
-        } else {
-          days[keys0] = new Day(key0);
-          days[key0].activities.push(this);
-        }
+        var key0 = this.startDate.format('YYYY-MM-DD');
+          if(days[key0]) {
+            days[key0].activities.push(this);
+          } else {
+            days[key0] = new Day(key0);
+            days[key0].activities.push(this);
+          }
+        var key1 = this.endDate.format('YYYY-MM-DD');
+          if(days[key1]) {
+            days[key1].activities.push(this);
+          } else {
+            days[key1] = new Day(key1);
+            days[key1].activities.push(this);
+          }
+      } else {
+        var key0 = this.startDate.format('YYYY-MM-DD');
+          if(days[key0]) {
+            days[key0].activities.push(this);
+          } else {
+            days[key0] = new Day(key0);
+            days[key0].activities.push(this);
+          }
+      }
     }
 
     this.category = function(category) {
@@ -64,7 +66,8 @@ var Activity = function(id, category, startDate, endDate) {
         categories[key].activities.push(this);
       }
 
-      this.category = category;
+      this.category = categories[key];
+
     };
 
     this.category(category);
@@ -80,96 +83,76 @@ Activity.prototype.getAsObject = function() {
   }
 }
 
-categories = {};
-days = {};
-var Category = function(name) {
+Activity.prototype.getDuration = function(date) {
+  if(this.multiple) {
+    if(moment(date).isSame(this.startDate.format(), 'day')) {
+      return this.dur0;
+    } else {
+      return this.dur1;
+    }
+  } else {
+    return this.duration;
+  }
+}
+
+var Category = function(name, color) {
   this.name = name;
+  this.color = color || '#CCCCCC';
   this.activities = [];
 }
 
 var Day = function(date) {
+  this.date = date;
   this.activities = [];
 }
 
-var drawPie = function(date) {
-  this.durations = [];
-  this.activities = [];
-  if(days[date]) {
-    var a = days[date].activities;
-    for(var i = 0; i < a.length; i++) {
-      var activity = a[i];
-      if(a[i].multiple) {
-        if(moment(date).isSame(activity.startDate.format(), 'day')) {
-          this.activities.push(activity);
-          this.durations.push(activity.dur0);
-        } else {
-          this.activities.push(activity);
-          this.durations.push(a[i].dur1);
-        }
+/**
+* Use days[this.state.day.date.format('YYYY-MM-DD')].getFreeDuration()
+* to get the free available duration in the day
+*/
+Day.prototype.getFreeDuration = function() {
+  var duration = 0;
+
+  for(var i = 0; i < this.activities.length; i++) {
+    var a = this.activities[i];
+    if(a.multiple) {
+      if(moment(this.date).isSame(a.startDate.format(), 'day')) {
+        duration += a.dur0;
       } else {
-        this.activities.push(activity);
-        this.durations.push(a[i].duration);
+        duration += a.dur1;
       }
+    } else {
+      duration += a.duration;
     }
   }
+
+  return 86400000-duration;
 }
 
-var mockActivities = [
-  {
-    id: 0,
-    category: 'Default',
-    startDate: moment('2016-11-15 00:00:00'),
-    endDate: moment('2016-11-15 23:59:59'),
-    duration: function() {return this.endDate.subtract(this.startDate.hours()).hours() + 1;}
-  },
-  {
-    id: 1,
-    category: 'Sleep',
-    startDate: moment('2016-11-14 23:00:00'),
-    endDate: moment('2016-11-15 08:00:00'),
-    duration: function() {return moment.duration(this.endDate.diff(this.startDate)).asMilliseconds();}
-  }
-]
-
-var dummyActivities = [
-  {
-    id: 0,
-    category: 'Default',
-    duration: 5,
-    color: '#CCCCCC'
-  },
-  {
-    id: 1,
-    category: 'Sleep',
-    duration: 8,
-    color: '#00CC00'
-  },
-  {
-    id: 2,
-    category: 'Work',
-    duration: 8,
-    color: '#4CAF50'
-  },
-  {
-    id: 3,
-    category: 'Cooking',
-    duration: 1,
-    color: '#CC0000'
-  },
-  {
-    id: 4,
-    category: 'Watching tv',
-    duration: 2,
-    color: '#CCC000'
-  }
+var categories = {
+  default: new Category('default', '#CCCCCC'),
+  sleeping: new Category('sleeping', '#539492'),
+  cooking: new Category('cooking', '#a86b00'),
+  watchingtv: new Category('watchingTV', '#a21c2f'),
+  studying: new Category('studying', '#6323a6'),
+  coding: new Category('coding', '#00CC00')
+};
+var days = {};
+var activities = [
+  new Activity(0, 'sleeping', moment('2016-11-19 23:00:00'), moment('2016-11-20 08:00:00')),
+  new Activity(1, 'cooking', moment('2016-11-20 12:00:00'), moment('2016-11-20 13:00:00')),
+  new Activity(2, 'watchingTV', moment('2016-11-20 13:00:00'), moment('2016-11-20 15:30:00')),
+  new Activity(3, 'studying', moment('2016-11-20 16:00:00'), moment('2016-11-20 18:30:00')),
+  new Activity(4, 'coding', moment('2016-11-20 15:00:00'), moment('2016-11-20 19:00:00')),
+  new Activity(5, 'sleeping', moment('2016-11-20 23:00:00'), moment('2016-11-21 08:00:00'))
 ];
+
 var mockDay = {
-  date: moment(),
-  activities: dummyActivities
+  date: moment('2016-11-20')
 }
 
-var sliceColor = ['#FFFFFF', '#00CC00', '#4CAF50', '#CCCCCC'];
-var chart_wh = 250;
+var sliceColor = ['#CCCCCC', '#CC0000', '#4CAF50', '#00CC00', '#00CCCC'];
+var chart_wh = 260;
 
 export default class habit extends Component {
   constructor(props) {
@@ -182,14 +165,42 @@ export default class habit extends Component {
         colors: []
       },
       nav: 'loading',
-
     }
   }
 
+  getPie() {
+    var date = this.state.day.date.format('YYYY-MM-DD');
+    var durations = [86400000];
+    var activities = [new Activity(-1, 'default')];
+    var colors = ['#CCCCCC'];
+    if(days[date]) {
+      var a = days[date].activities;
+      for(var i = 0; i < a.length; i++) {
+        var activity = a[i];
+        var category = activity.category;
+        colors.push(activity.category.color);
+        if(a[i].multiple) {
+          if(moment(date).isSame(activity.startDate.format(), 'day')) {
+            durations.splice(i, 0, activity.dur0);
+            durations[durations.length-1] -= activity.dur0;
+          } else {
+            durations.splice(i, 0, activity.dur1);
+            durations[durations.length-1] -= activity.dur1;
+          }
+        } else {
+          durations.splice(i, 0, activity.duration);
+          durations[durations.length-1] -= activity.duration;
+        }
+        activities.splice(i, 0, activity);
+        colors.splice(i, 0, activity.category.color);
+      }
+    }
+    activities[activities.length-1].duration = durations[durations.length-1];
+    return {durations: durations, activities: activities, colors: colors};
+  }
+
   componentDidMount() {
-    this.getDurations();
     this.setState({nav: 'day'});
-    console.log('hey');
   }
 
   render() {
@@ -211,23 +222,57 @@ export default class habit extends Component {
     )
   }
 
+  pressActivity() {
+
+  }
+
+  renderActivity(id) {
+    var activity = activities[id];
+    return (
+      <View style={styles.container}>
+        <View>
+          <Text>
+            Category {activity.category.name}
+          </Text>
+        </View>
+        <View>
+          <Text>
+            Start Date: {activity.startDate.format('ll')}
+          </Text>
+        </View>
+        <View>
+          <Text>
+            End Date: {activity.endDate.format('ll')}
+          </Text>
+        </View>
+        <View>
+          <Text>
+            Save btn ///// Delete btn
+          </Text>
+        </View>
+      </View>
+    )
+  }
+
   renderDay() {
+    var pie = this.getPie();
+    var durations = pie.durations;
     return (
       <View style={styles.container}>
         <View style={{flex:1, justifyContent: 'center', alignItems: 'center', paddingTop: 32}}>
-          <Text style={{fontSize: 18}}>
-            {this.state.day.date.format('LL')} {mockActivities[1].duration()}
+          <Text style={[{fontSize: 18}, styles.defaultFont]}>
+            {this.state.day.date.format('LL')}
           </Text>
         </View>
         <View style={{flex: 4, justifyContent: 'center'}}>
           <PieChart
             chart_wh={chart_wh}
-            series={this.state.pie.durations}
-            sliceColor={this.state.pie.colors}
+            series={durations}
+            sliceColor={pie.colors}
           />
         </View>
         <View style={{flex: 4, alignSelf: 'flex-start', paddingLeft: 30}}>
-          {this.categoryList()}
+          {this.categoryList(pie, this)}
         </View>
       </View>
     );
@@ -272,7 +317,7 @@ export default class habit extends Component {
   renderWeek() {
 
   }
-
+/*
   getDurations() {
     var durations = [],
         colors    = [],
@@ -285,38 +330,27 @@ export default class habit extends Component {
     this.setState({pie: {durations: durations, colors: colors}});
 
   }
-
+  */
 
   addActivity(date, category) {
   }
 
-  getDurationsAlt(date) {
-    var durations = [],
-        colors    = [],
-        activities = mockActivities;
-
-    for(var i = 0; i < mockActivities.length; i++) {
-
-    }
-  }
-
-  categoryList() {
+  categoryList(pie, parent) {
     var dot = function(color) {
       return (
         <View style={{height: 20, width: 20, borderRadius: 20, backgroundColor: color}}></View>
       )
     }
-    return this.state.day.activities.map(function(obj, index){
+    return pie.activities.map(function(obj, index){
       return (
         <View key={obj.id} style={{flexDirection: 'row', alignItems: 'center', padding: 3}}>
-          {dot(obj.color)}
-
-          <Text style={{paddingLeft: 6}}>
-            {obj.category}
+          {dot(obj.category.color)}
+          <Text style={[{fontSize: 17, paddingLeft: 6}, styles.defaultFont]}>
+            {obj.category.name}
           </Text>
 
-          <Text>
-            :{obj.duration}
+          <Text style={[{fontSize: 17}, styles.defaultFont]}>
+            : {obj.duration ? moment.duration(obj.getDuration(parent.state.day.date.format('YYYY-MM-DD')), 'milliseconds').humanize() : 0}
           </Text>
         </View>
       )
@@ -329,10 +363,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#282828',
   },
   defaultFont: {
-    color: '#aeaeae'
+    color: '#aeaeae',
+    fontWeight: '400'
   },
   welcome: {
     fontSize: 20,
