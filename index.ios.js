3e7dbe18a59a7f3f308b61c8e4b3e9569ae88064
erikjuhani <erik.kinn@gmail.com>
2016-11-26 14:35:04 +0200

    Added navigation and day selection calendar.

--- index.ios.js
+++ index.ios.js
@@ -11,11 +11,30 @@ import {
   Text,
   View,
   ListView,
-  TouchableHighlight
+  TouchableHighlight,
+  TextInput,
+  Navigator,
+  ScrollView,
+  Dimensions
 } from 'react-native';
 
 import moment from 'moment';
 import PieChart from 'react-native-pie-chart';
+import Swiper from 'react-native-swiper';
+import Calendar from 'react-native-calendar';
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+/*
+const firebaseConfig = {
+  apiKey: "AIzaSyC2MswomuyAqgT8YidVIOEDW3eSdPdnFy0",
+  authDomain: "habit-7e89d.firebaseapp.com",
+  databaseURL: "https://habit-7e89d.firebaseio.com",
+  storageBucket: "habit-7e89d.appspot.com",
+  messagingSenderId: "108356244031"
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+*/
 
 var Activity = function(id, category, startDate, endDate) {
     this.id = id;
@@ -106,6 +125,14 @@ var Day = function(date) {
   this.activities = [];
 }
 
+var User = function(name, email) {
+  this.name = name;
+  this.email = email;
+  this.days;
+  this.activities;
+  this.categories;
+}
+
 /**
 * Use days[this.state.day.date.format('YYYY-MM-DD')].getFreeDuration()
 * to get the free available duration in the day
@@ -139,17 +166,13 @@ var categories = {
 };
 var days = {};
 var activities = [
-  new Activity(0, 'sleeping', moment('2016-11-19 23:00:00'), moment('2016-11-20 08:00:00')),
+  new Activity(0, 'sleeping', moment('2016-11-22 23:00:00'), moment('2016-11-23 08:00:00')),
   new Activity(1, 'cooking', moment('2016-11-20 12:00:00'), moment('2016-11-20 13:00:00')),
   new Activity(2, 'watchingTV', moment('2016-11-20 13:00:00'), moment('2016-11-20 15:30:00')),
   new Activity(3, 'studying', moment('2016-11-20 16:00:00'), moment('2016-11-20 18:30:00')),
   new Activity(4, 'coding', moment('2016-11-20 15:00:00'), moment('2016-11-20 19:00:00')),
   new Activity(5, 'sleeping', moment('2016-11-20 23:00:00'), moment('2016-11-21 08:00:00'))
 ];
-
-var mockDay = {
-  date: moment('2016-11-20')
-}
 
 var sliceColor = ['#CCCCCC', '#CC0000', '#4CAF50', '#00CC00', '#00CCCC'];
 var chart_wh = 260;
@@ -159,17 +182,22 @@ export default class habit extends Component {
     super(props);
     //const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
-      day: mockDay,
+      selectedDate: moment(),
       pie: {
         durations: [],
         colors: []
       },
       nav: 'loading',
+      startTime: null,
+      endTime: null,
+      running: false,
+      timeElapsed: null,
+      showCalendar: false,
     }
   }
 
   getPie() {
-    var date = this.state.day.date.format('YYYY-MM-DD');
+    var date = this.state.selectedDate.format('YYYY-MM-DD');
     var durations = [86400000];
     var activities = [new Activity(-1, 'default')];
     var colors = ['#CCCCCC'];
@@ -204,12 +232,45 @@ export default class habit extends Component {
   }
 
   render() {
+    return (
+      <Swiper showsButtons={true}>
+        {this.renderProfile()}
+        {this.renderDay()}
+        {this.renderTimer()}
+      </Swiper>
+    )
+    /*
     if(this.state.nav === 'day') {
       return this.renderDay();
     } else if(this.state.nav === 'timer') {
       return this.renderTimer();
+    } else if(this.state.nav === 'newCat') {
+      return this.renderNewCategory();
     }
     return this.loading();
+    */
+  }
+
+  renderProfile() {
+    return (
+      <View style={[styles.container, {paddingTop: 20, alignItems: 'center'}]}>
+        <View style={{flex:.1}}>
+          <Text style={styles.defaultFont}>
+            "Navigation bar"
+          </Text>
+        </View>
+        <View style={{flex:.2}}>
+          <Text style={styles.defaultFont}>
+            Avatar or something
+          </Text>
+        </View>
+        <View style={{flex:.7}}>
+          <Text style={styles.defaultFont}>
+            Statistics maybe
+          </Text>
+        </View>
+      </View>
+    )
   }
 
   loading() {
@@ -224,6 +285,78 @@ export default class habit extends Component {
 
   pressActivity() {
 
+  }
+
+  newCategory() {
+    var category = {};
+    category[this.state.catName] = this.state.catName;
+  }
+
+  renderNewCategory() {
+    return (
+      <View style={{flex:1, padding: 20}}>
+        <View>
+          <Text>
+            Category name:
+          </Text>
+          <TextInput
+            style={{height: 40, borderWidth: 2, padding: 10}}
+            onChangeText={(catName) => this.setState({catName})}
+            value={this.state.catName}
+          />
+        </View>
+        <View>
+          <Text>
+            {categories.playing}
+          </Text>
+        </View>
+        <View>
+          <Text>
+            Category color:
+          </Text>
+          <TextInput
+            style={{height: 40, borderWidth: 2, padding: 10}}
+            onChangeText={(catColor) => this.setState({catColor})}
+            value={this.state.catColor}
+          />
+        </View>
+        <TouchableHighlight
+          style={[timerStyles.sqrButton, {backgroundColor: '#00CC00'}]}
+          onPress={this.newCategory.bind(this)}
+        >
+          <Text>
+            SAVE
+          </Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+
+  renderNewActivity() {
+    return (
+      <View style={styles.container}>
+        <View>
+          <Text>
+            Add new activity
+          </Text>
+        </View>
+        <View>
+          <Text>
+            Start Time
+          </Text>
+        </View>
+        <View>
+          <Text>
+            End Time or Duration
+          </Text>
+        </View>
+        <View>
+          <Text>
+            Save btn ///// Delete btn
+          </Text>
+        </View>
+      </View>
+    )
   }
 
   renderActivity(id) {
@@ -247,7 +380,7 @@ export default class habit extends Component {
         </View>
         <View>
           <Text>
-            Save btn ///// Delete btn
+            Edit btn ///// Delete btn
           </Text>
         </View>
       </View>
@@ -257,29 +390,102 @@ export default class habit extends Component {
   renderDay() {
     var pie = this.getPie();
     var durations = pie.durations;
+    var offset = {x:0, y:285}
     return (
       <View style={styles.container}>
-        <View style={{flex:1, justifyContent: 'center', alignItems: 'center', paddingTop: 32}}>
-          <Text style={[{fontSize: 18}, styles.defaultFont]}>
-            {this.state.day.date.format('LL')}
-          </Text>
-        </View>
-        <View style={{flex: 4, justifyContent: 'center'}}>
-          <PieChart
-            chart_wh={chart_wh}
-            series={durations}
-            sliceColor={pie.colors}
+        <ScrollView
+          ref={(scrollView) => { _scrollView = scrollView; }}
+          contentOffset={offset}
+          scrollEnabled={false}
+          centerContent={false}
+          contentInset={{bottom: 315}}
+          style={{paddingTop:20}}
+        >
+          <Calendar
+            ref='calendar'
+            eventDates={['2016-11-19', '2016-11-20', '2016-11-21', '2016-11-22']}
+            events={[{date: '2016-11-20', hasEventCircle: {backgroundColor: 'powderblue'}}]}
+            showControls
+            titleFormat={'MMMM YYYY'}
+            prevButtonText={'Prev'}
+            nextButtonText={'Next'}
+            onDateSelect={(date) => this.setState({ selectedDate: moment(date) })}
+            onTouchPrev={() => console.log('Back TOUCH')}     // eslint-disable-line no-console
+            onTouchNext={() => console.log('Forward TOUCH')}  // eslint-disable-line no-console
+            onSwipePrev={() => console.log('Back SWIPE')}     // eslint-disable-line no-console
+            onSwipeNext={() => console.log('Forward SWIPE')}  // eslint-disable-line no-console
           />
-        </View>
-        <View style={{flex: 4, alignSelf: 'flex-start', paddingLeft: 30}}>
-          {this.categoryList(pie, this)}
-        </View>
+          <TouchableHighlight
+            style={{borderWidth: 2, paddingTop: 50}}
+            onPress={() => {
+              if(this.state.showCalendar) {
+                _scrollView.scrollTo({y: 285});
+                this.state.showCalendar = false;
+              } else {
+                _scrollView.scrollTo({y: 0});
+                this.state.showCalendar = true;
+              }
+            }}
+          >
+            <View></View>
+          </TouchableHighlight>
+          <View style={{alignSelf:'center', padding: 20}}>
+            <TouchableHighlight
+              onPress={function(){return;}}
+              style={{borderRadius: 10, padding: 4}}
+            >
+              <Text style={[{fontSize: 18}, styles.defaultFont]}>
+                {this.state.selectedDate.format('LL')}
+              </Text>
+            </TouchableHighlight>
+          </View>
+          <View style={{alignSelf:'center'}}>
+            <PieChart
+              chart_wh={chart_wh}
+              series={durations}
+              sliceColor={pie.colors}
+            />
+          </View>
+          <View style={{paddingLeft: 30, paddingTop: 20}}>
+            {this.categoryList(pie, this)}
+          </View>
+        </ScrollView>
       </View>
     );
+    this.refs._scrollView.scrollTo({x: 0, y: 265, animated: true})
   }
 
-  startStopButton() {
+  pressTimerButton() {
+    if(this.state.running) {
+      clearInterval(this.interval);
+      this.setState({running:false, endTime: moment()});
+      return;
+    } else if(this.state.startTime){
+      this.setState({startTime: this.state.startTime});
+    } else {
+      this.setState({startTime: moment()});
+    }
 
+    this.interval = setInterval(() => {
+      this.setState({
+        timeElapsed: moment() - this.state.startTime,
+        running: true
+      });
+    }, 30);
+  }
+
+  pressClearButton() {
+    if(!this.state.running) {
+      this.setState({startTime: null, running: false, timeElapsed: null, endTime: null});
+    }
+  }
+
+  pressSaveButton() {
+    if(this.state.startTime && !this.state.running && this.state.timeElapsed >= 120000) {
+      new Activity(6, 'cooking', this.state.startTime, moment());
+      //activities.push(activity);
+      this.setState({startTime: null, running: false, timeElapsed: null, endTime: null, nav:'day'});
+    }
   }
 
   renderTimer() {
@@ -290,25 +496,54 @@ export default class habit extends Component {
             Start Activity
           </Text>
         </View>
-        <View style={timerStyles.buttonWrapper}>
-          <View style={timerStyles.button}>
-            <Text style={timerStyles.buttonFont}>
-              START
-            </Text>
-          </View>
-        </View>
-        <View style={timerStyles.timer}>
-          <Text style={[styles.defaultFont, {fontSize: 18}]}>
-            00:00:00
-            {moment().toString()}
+
+        <View style={{alignItems: 'center', paddingBottom: 10}}>
+          <Text style={[styles.defaultFont, {fontSize: 18, width: 165}]}>
+            Start time: {this.state.startTime ? this.state.startTime.format('HH:mm:ss') : '00:00:00'}
           </Text>
         </View>
+        <View style={timerStyles.timer}>
+          <Text style={[styles.defaultFont, {fontSize: 24, width: 163}]}>
+            {this.state.timeElapsed ? moment.utc(this.state.timeElapsed).format('HH:mm:ss.SSS') : '00:00:00.000'}
+          </Text>
+        </View>
+        <View style={timerStyles.buttonWrapper}>
+          <TouchableHighlight
+            style={this.state.running ? timerStyles.stopButton : timerStyles.startButton}
+            onPress={this.pressTimerButton.bind(this)}
+          >
+            <Text style={timerStyles.buttonFont}>
+              {this.state.running ? 'STOP' : this.state.timeElapsed ? 'CONTINUE' : 'START'}
+            </Text>
+          </TouchableHighlight>
+        </View>
         <View style={timerStyles.footer}>
-          <View style={timerStyles.sqrButton}>
-            <Text style={[styles.defaultFont,{fontSize: 20, fontWeight: '400'}]}>
+          <TouchableHighlight
+            style={timerStyles.sqrButton}
+            onPress={this.pressSaveButton.bind(this)}
+          >
+            <Text style={[styles.defaultFont,{fontSize: 20, fontWeight: '300'}]}>
               SAVE
             </Text>
-          </View>
+          </TouchableHighlight>
+          <TouchableHighlight
+            style={timerStyles.sqrButton}
+            onPress={this.pressClearButton.bind(this)}
+          >
+            <Text style={[styles.defaultFont,{fontSize: 20, fontWeight: '300'}]}>
+              CLEAR
+            </Text>
+          </TouchableHighlight>
+        </View>
+        <View style={timerStyles.footer}>
+          <TouchableHighlight
+            style={timerStyles.sqrButton}
+            onPress={function(){}}
+          >
+            <Text style={[styles.defaultFont,{fontSize: 20, fontWeight: '300'}]}>
+              ADD
+            </Text>
+          </TouchableHighlight>
         </View>
       </View>
     )
@@ -335,6 +570,10 @@ export default class habit extends Component {
   addActivity(date, category) {
   }
 
+  pressActivityListItem() {
+    return;
+  }
+
   categoryList(pie, parent) {
     var dot = function(color) {
       return (
@@ -343,16 +582,20 @@ export default class habit extends Component {
     }
     return pie.activities.map(function(obj, index){
       return (
-        <View key={obj.id} style={{flexDirection: 'row', alignItems: 'center', padding: 3}}>
-          {dot(obj.category.color)}
-          <Text style={[{fontSize: 17, paddingLeft: 6}, styles.defaultFont]}>
-            {obj.category.name}
-          </Text>
-
-          <Text style={[{fontSize: 17}, styles.defaultFont]}>
-            : {obj.duration ? moment.duration(obj.getDuration(parent.state.day.date.format('YYYY-MM-DD')), 'milliseconds').humanize() : 0}
-          </Text>
-        </View>
+        <TouchableHighlight key={index}
+          onPress={parent.pressActivityListItem.bind(parent)}
+          style={{borderRadius: 10, padding: 2}}
+        >
+          <View key={obj.id} style={{flexDirection: 'row', padding: 3}}>
+            {dot(obj.category.color)}
+            <Text style={[{fontSize: 17, paddingLeft: 6}, styles.defaultFont]}>
+              {obj.category.name}
+            </Text>
+            <Text style={[{fontSize: 17}, styles.defaultFont]}>
+              : {obj.duration ? moment.duration(obj.getDuration(parent.state.selectedDate.format('YYYY-MM-DD')), 'milliseconds').humanize() : 0}
+            </Text>
+          </View>
+        </TouchableHighlight>
       )
     });
   }
@@ -361,8 +604,8 @@ export default class habit extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'stretch',
+    justifyContent: 'space-between',
     backgroundColor: '#282828',
   },
   defaultFont: {
@@ -389,7 +632,8 @@ const timerStyles = StyleSheet.create({
   header: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    paddingTop: 12
   },
   textDisplay: {
     fontSize: 18,
@@ -398,15 +642,25 @@ const timerStyles = StyleSheet.create({
     flex: 2,
     alignItems: 'center'
   },
-  button: {
+  startButton: {
     backgroundColor: '#545454',
     borderColor: '#aeaeae',
     justifyContent: 'center',
     alignItems: 'center',
     borderWidth: 2,
-    borderRadius: 90,
-    width: 180,
-    height: 180
+    borderRadius: 80,
+    width: 160,
+    height: 160
+  },
+  stopButton: {
+    backgroundColor: '#a21c2f',
+    borderColor: '#aeaeae',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 2,
+    borderRadius: 80,
+    width: 160,
+    height: 160
   },
   buttonFont: {
     fontSize: 16,
@@ -414,21 +668,22 @@ const timerStyles = StyleSheet.create({
     fontWeight: '400'
   },
   sqrButton: {
-    width: 200,
+    width: 120,
     height: 50,
     borderWidth: 2,
     borderColor: '#aeaeae',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#418b10'
+    backgroundColor: '#545454'
   },
   timer: {
-    flex: 2,
+    flex: 1,
     alignItems: 'center'
   },
   footer: {
-    flex: 2,
-    alignItems: 'center'
+    flex: 1,
+    justifyContent: 'space-around',
+    flexDirection: 'row'
   }
 
 });
